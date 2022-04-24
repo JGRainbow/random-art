a7395ac7312618f2f00b550a7c4fd84dbf5b7ea3
Jacob Rainbow <jacobrainbow@Jacobs-MacBook-Pro.local>
2022-04-24 14:34:22 +0100

    add precommit hooks

--- app/canvas.py
+++ app/canvas.py
@@ -1,14 +1,26 @@
 from typing import Tuple
 
+import numpy as np
+
 from app.turtle import Turtle
 from app.coord import Coord
 
 
 class Canvas:
+    """
+    Board for simulating Turtle movements. 
+    ---
+    Grid size is (height, width)
+    """
 
     def __init__(self, turtle: Turtle, grid_size: Tuple[int] = (10, 10)):
         self.turtle = turtle
         self.grid_size = grid_size
+        self.grid = self._init_grid(self.grid_size)
+
+    @staticmethod
+    def _init_grid(grid_size: Tuple[int]):
+        return np.zeros(grid_size)
 
     def step(self):
         pass
